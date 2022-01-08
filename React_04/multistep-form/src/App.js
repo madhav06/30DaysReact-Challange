014d470711172065d254e617ae608d30d9941382
madhav06 <mnandan06@gmail.com>
2022-01-08 23:00:51 +0530

    first commit

---
+++ React_04/multistep-form/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import React from "react";
+import { UseForm } from "./components/UseForm";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <UseForm />
+    </div>
+  );
+}
+
+export default App;
