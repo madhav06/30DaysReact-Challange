7dd23dac67a51aa8d09980869d7f01ecc828a641
madhav06 <mnandan06@gmail.com>
2022-01-13 13:02:59 +0530

    --fix update module 03

---
+++ REACT_03/NOTEZIPPER/backend/data/notes.js
@@ -0,0 +1,20 @@
+const notes = [
+  {
+    _id: "1",
+    title: "Covid Cases in Maharashtra rising !",
+    content:
+      "With the rise in omnicrone cases in districts of maharashtra, the Maha Government is imposing night-curfew for 3 weeks",
+    category: "News",
+  },
+
+  {
+    _id: "2",
+    title:
+      "JPP defeted Patna Pirated with 22-12 Points. They had amazing fight!",
+    content:
+      "In Tonight match at Gachibowli stadium, Hyderabad Jaipur Pink Panther Pro Kabbadi Team defeted badly the winning team, Patna Pirates and showed that they are marching ahead to get the trophy this year.",
+    category: "Sports",
+  },
+];
+
+module.exports = notes;
---
+++ REACT_03/NOTEZIPPER/backend/server.js
@@ -0,0 +1,26 @@
+//server.js
+
+const express = require("express");
+const notes = require("./data/notes");
+const dotenv = require("dotenv");
+const app = express();
+
+dotenv.config();
+
+// Routes
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((n) => n._id === req.params.id);
+  res.send(note);
+});
+
+const PORT = process.env.PORT || 9002;
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
