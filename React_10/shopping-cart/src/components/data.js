f5a435ef83ae60a6dd84c1baabc4ec799aaebfe5
madhav06 <mnandan06@gmail.com>
2022-02-10 13:36:22 +0530

    Simple Shopping Cart App

---
+++ React_10/shopping-cart/src/components/data.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    title: "Samsung Galaxy S8",
+    price: 599.99,
+    img: "https://i.pinimg.com/474x/51/b4/d8/51b4d874e31f10e0e4b61c283745064a.jpg",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "google pixel ",
+    price: 499.99,
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583371867/phone-1_gvesln.png",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "Xiaomi Redmi Note 2",
+    price: 699.99,
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368224/phone-3_h2s6fo.png",
+    amount: 1,
+  },
+];
