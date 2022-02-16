a1da679d8a98d42bbc76a3ae1c6ff55ce6b1df49
madhav06 <mnandan06@gmail.com>
2022-02-16 22:13:39 +0530

    Building custom search list filter

---
+++ React_14/custom-search-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import DynamicSearch from "./components/FilterSearch";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <DynamicSearch />
+    </div>
+  );
+}
+
+export default App;
