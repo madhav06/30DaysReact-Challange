1dc6f91efbacebd4529a8d2add10daa4729ad055
madhav06 <mnandan06@gmail.com>
2022-04-21 01:33:46 +0530

    Services and solutions added

---
+++ React_01/landing-pages-react/madhav-landing/src/components/Presentation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Presentation() {
+    return(
+        <div id='presentaion'>
+            <h1>Lorem Ipseum</h1>
+            <p>lorem ipsum dolor elit</p>
+        </div>
+    )
+}
+
+export default Presentation
