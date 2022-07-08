dc653f86eb6761f652ae1c7464fa48b249240cbe
madhav06 <mnandan06@gmail.com>
2022-07-08 12:53:12 +0530

    Working W/ Components

--- Day02/react_with_hooks/src/Products.js
+++ Day02/react_with_hooks/src/Products.js
@@ -19,7 +19,7 @@ const Products = () => {
           imageUrl: "http://loremflickr.com/150/150?random=2",
           productName: "Product 2",
           releasedDate: "October 31, 2016",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean porttitor, tellus laoreet venenatis facilisis, enim ex faucibus nulla, id rutrum ligula purus sit amet mauris. ",        
+          description: "Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc, litot Europa usa li sam vocabular. Li lingues differe solmen in li grammatica, li pronunciation e li plu commun vocabules. ",        
           rating: 2,
           numOfReviews: 12          
       },
@@ -27,7 +27,7 @@ const Products = () => {
           imageUrl: "http://loremflickr.com/150/150?random=3",
           productName: "Product 3",
           releasedDate: "July 30, 2016",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean porttitor, tellus laoreet venenatis facilisis, enim ex faucibus nulla, id rutrum ligula purus sit amet mauris. ",        
+          description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, ",        
           rating: 5,
           numOfReviews: 2
       }
